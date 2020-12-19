2026ae3e52e37345c6bb86447974390e7962f6b3
Roman Yakovliev <RomaYakovlev@hotmail.com>
2020-12-19 19:53:35 +0200

    UBUNTU IS PIECE OF ****

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,3 +1,34 @@
 // The face detection does not work on all browsers and operating systems.
 // If you are getting a `Face detection service unavailable` error or similar,
 // it's possible that it won't work for you at the moment.
+
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+const faceCanvas = document.querySelector('.face');
+const faceCtx = faceCanvas.getContext('2d');
+const faceDetector = new window.FaceDetector();
+// console.log(video, canvas, faceCanvas, faceDetector);
+
+// Write a function that will populate users video
+async function populateVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: {width: 1280, height: 720 }
+        // video: true,
+    });
+    video.srcObject = stream;
+    await video.play();
+    // size the canvases t be the same size as the video
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    faceCanvas.width = video.videoWidth;
+    faceCanvas.height = video.videoHeight;
+}
+
+async function detect() {
+    const faces = await faceDetector.detect(video)
+    console.log(faces);
+}
+
+// 00:23:00
+// populateVideo().then(detect);
