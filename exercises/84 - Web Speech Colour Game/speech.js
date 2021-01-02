c3dccec2dd61f07a97e3bdb635dba4057cd652cf
veilvokay <romayakovlev@hotmail.com>
2021-01-02 19:17:32 +0200

    84, 85 - done; course completed

---
+++ exercises/84 - Web Speech Colour Game/speech.js
@@ -0,0 +1,31 @@
+import {handleResult} from './handlers';
+import { colorsByLength, isDark } from './colors';
+
+const colorsEl = document.querySelector('.colors');
+
+function displayColors(colors) {
+    return colors.map(color => {
+        return `<span class="color ${color} ${isDark(color) ? 'dark' : ''}" style="background: ${color}">${color}</span>`;
+    }).join('');
+}
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+function start() {
+    // see if their browser supports this
+    if(!('SpeechRecognition' in window)) {
+        console.log('Sorry your borwser doesnt support speech reco.');
+        return
+    }
+    // it does work
+    console.log('Starting...');
+    // make a new speech reco
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.onresult = handleResult;
+    recognition.start(); 
+}
+
+start();
+colorsEl.innerHTML = displayColors(colorsByLength);
